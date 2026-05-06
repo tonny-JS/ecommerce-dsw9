2c744010e8aa86f6c0d7eb698c6bd8b382407b7f
tonny-JS <jack.salazar@utp.ac.pa>
2026-05-06 00:22:15 +0000

    feat: controladores y rutas completos

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,8 @@
+// routes/products.js
 const express = require('express');
 const router  = express.Router();
-// Rutas por implementar
+const ctrl    = require('../controllers/productController');
+router.get('/',             ctrl.getHomePage);
+router.get('/products',     ctrl.getAllProducts);
+router.get('/products/:id', ctrl.getProductById);
 module.exports = router;
